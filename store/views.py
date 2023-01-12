b373f9facf436d2f161dc68ec7d0d35f6ba4cb59
Sanowar Dewan <sanowardwnofficial@gmail.com>
2023-01-12 20:48:36 +0600

    category fetch

--- store/views.py
+++ store/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def category(request):
+    category = Category.objects.all()
+    context = {'categories':category}
+    return render(request, "store/category.html", context)
 
 def home(request):
     return render(request, "store/index.html")
